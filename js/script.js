3e344387377e2c64d5d91f6ee74585fa7486e5b3
GreyCat20142017 <33216844+GreyCat20142017@users.noreply.github.com>
2017-12-03 12:40:09 +0300

    Все подряд по мере возможности
    Борьба с переполнениями, борьба с недоступностью картинок, возврат подменю и форм и т.д.

--- js/script.js
+++ js/script.js
@@ -1,5 +1,38 @@
 function RedrawOutput() {
-	 document.getElementById('id-filter-range-output').value='Цена: '+
-	 	Math.min(document.getElementById('id-filter-range-1').value, document.getElementById('id-filter-range-2').value)+' руб. - '+
-	 	Math.max(document.getElementById('id-filter-range-1').value, document.getElementById('id-filter-range-2').value)+' руб.';	 	
+	document.getElementById('id-filter-range-output').value='Цена: '+
+	Math.min(document.getElementById('id-filter-range-1').value, document.getElementById('id-filter-range-2').value)+' руб. - '+
+	Math.max(document.getElementById('id-filter-range-1').value, document.getElementById('id-filter-range-2').value)+' руб.';	 	
 }
+
+var feedbackLink = document.querySelector(".map-info-feedback");
+var feedbackForm = document.querySelector(".form-feedback");
+var feedbackField = feedbackForm.querySelector(".form-feedback-name");
+var feedbackClose = feedbackForm.querySelector(".form-feedback-close");
+
+var darkenArea = document.createElement('div');
+
+window.addEventListener("keydown", function (e) {
+	if (e.keyCode === 27) {
+		if (feedbackForm.classList.contains("gllacy-flex-show")) {
+			feedbackForm.classList.remove("gllacy-flex-show");     
+		}
+		darkenArea.parentNode.removeChild(darkenArea);
+	}
+});
+
+
+feedbackLink.addEventListener("click", function (e) {	
+	e.preventDefault();	    
+	darkenArea.className = "darken";
+	document.body.appendChild(darkenArea);
+	feedbackForm.classList.add("gllacy-flex-show");
+	feedbackField.focus();
+});
+
+feedbackClose.addEventListener("click", function (e) {
+	e.preventDefault();
+	if (feedbackForm.classList.contains("gllacy-flex-show")) {
+		feedbackForm.classList.remove("gllacy-flex-show");     
+	}	
+	darkenArea.parentNode.removeChild(darkenArea);
+});
